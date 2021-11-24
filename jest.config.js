9d69a9b9abda763d74962b1b08d785e1086c227a
Kevin Swiber <kswiber@gmail.com>
2021-11-24 15:23:38 -0800

    Trying to hack stdin doesn't work.

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,8 @@ module.exports = {
   testEnvironment: "node",
   modulePathIgnorePatterns: ["<rootDir>/.aws-sam", "<rootDir>/__tests__/utils"],
   moduleNameMapper: {
+    "^@stoplight/spectral-ruleset-bundler/(.*)":
+      "<rootDir>/node_modules/@stoplight/spectral-ruleset-bundler/dist/$1",
     "^nimma/fallbacks$":
       "<rootDir>/node_modules/nimma/dist/cjs/fallbacks/index.js",
     "^nimma/legacy$": "<rootDir>/node_modules/nimma/dist/legacy/cjs/index.js",
